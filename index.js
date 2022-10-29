54311bc92aebc51560f818f970778d5a41b5f1db
Hudhayfah Ismail <hoismail2017@gmail.com>
2022-10-29 08:11:41 +0100

    first

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config()
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    const userObj = {
+        slackUsername: 'Ismail Hudhayfah',
+        backend: true,
+        age: 22,
+        bio: 'I am Ismail Hudhayfah, a backend developer possionate in proferring solution to problems and working in a team to achieve the best possible outcome'
+    }
+
+    res.status(200).json(userObj)
+})
+
+app.listen(process.env.PORT || 8000  , ()=> console.log("Stage1_Started"))
